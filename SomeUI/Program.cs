3b23a6e906a7f72dba35df83a214484fe5f1becb
Arun Thomas <ArunThomas04@gmail.com>
2020-05-15 18:59:00 +0530

    Added InsertMulipleDifferentObjects, InsertMultipleSamurais

--- SomeUI/Program.cs
+++ SomeUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,90 @@ using System.Threading.Tasks;
 
 namespace SomeUI
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            InsertSamurai();
+            InsertMultipleSamurais();
+            InsertMulipleDifferentObjects();
+            Console.ReadLine();
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai()
+            {
+                Name = "Arun"
+            };
+
+            using (var context = new DatabaseContext())
+            {
+                //*** either of the below add methods can be used
+                //context.Add(samurai);
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+
+        private static void InsertMultipleSamurais()
+        {
+            var samurai1 = new Samurai()
+            {
+                Name = "S1"
+            };
+            var samurai2 = new Samurai()
+            {
+                Name = "S2"
+            };
+            using (var context = new DatabaseContext())
+            {
+                //*** either of the below add methods can be used
+                //context.Add(samurai);
+                context.Samurais.AddRange(samurai1, samurai2);
+                context.SaveChanges();
+            }
+
+
+            var samurai3 = new Samurai()
+            {
+                Name = "S3"
+            };
+            var samurai4 = new Samurai()
+            {
+                Name = "S4"
+            };
+            var sam = new List<Samurai>();
+            sam.Add(samurai3);
+            sam.Add(samurai4);
+            using (var context = new DatabaseContext())
+            {
+                context.Samurais.AddRange(sam);
+                context.SaveChanges();
+            }
+
+
+        }
+
+
+        private static void InsertMulipleDifferentObjects()
+        {
+            var samurai = new Samurai()
+            {
+                Name = "S5"
+            };
+            var battle = new Battle()
+            {
+                Name = "First Battle",
+                StartDate = DateTime.Now.AddYears(-100),
+                EndDate = DateTime.Now.AddYears(-99)
+            };
+
+            using (var context = new DatabaseContext())
+            {
+                context.AddRange(samurai, battle);
+                context.SaveChanges();
+            }
         }
     }
 }
